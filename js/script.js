6d9b30b173cef8dc93e3b39b81da3b73d947e573
omareldesokyy <omareldesokyy@gmail.com>
2024-06-08 23:21:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -29,10 +29,73 @@ const toastAlert = document.getElementById('liveToast');
 const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastAlert)
 
 var users = [];
+if (localStorage.getItem('status') != null){
+    if(localStorage.getItem('status').includes('login')){
+        localStorage.getItem('status').split(',')[1]
+    
+        loginSystem.classList.add('d-none');
+        data.classList.remove('d-none');
+        infoDisplay( localStorage.getItem('status').split(',')[1]);
+        info.classList.replace('d-none', 'd-flex');
+        getData();
+        clearLogInData();
+    }else{
+    
+    }
+}
+
 
 if (localStorage.getItem('users') != null) {
     users = JSON.parse(localStorage.getItem('users'));
 }
+
+
+
+var emailValidate=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+var passwordValidate = /^.{8,}$/;
+var nameValidate = /^.{4,}$/;
+
+suName.addEventListener('focusout',function(){
+    if(!nameValidate.test(suName.value)){
+        suName.classList.add('is-invalid')
+        suName.classList.remove('is-valid')
+
+        Alert('danger','Name is invalid','Should be 4 characters or more')
+    }
+    else{
+        suName.classList.add('is-valid')
+        suName.classList.remove('is-invalid')
+
+    }
+})
+
+suEmail.addEventListener('focusout',function(){
+    if(!emailValidate.test(suEmail.value)){
+        suEmail.classList.add('is-invalid')
+        suEmail.classList.remove('is-valid')
+
+        Alert('danger','Email is invalid','Email does not meet internet email format standards or the email does not exist')
+    }
+    else{
+        suEmail.classList.add('is-valid')
+        suEmail.classList.remove('is-invalid')
+
+    }
+})
+
+suPassword.addEventListener('focusout',function(){
+    if(!passwordValidate.test(suPassword.value)){
+        suPassword.classList.add('is-invalid')
+        suPassword.classList.remove('is-valid')
+
+        Alert('danger','Password is invalid','Should be 8 characters or more')
+    }
+    else{
+        suPassword.classList.add('is-valid')
+        suPassword.classList.remove('is-invalid')
+
+    }
+})
 
 function Alert(type, tite, message) {
     toastAlert.innerHTML = `
@@ -47,50 +110,66 @@ function Alert(type, tite, message) {
 
 function signUp() {
 
-    const searchForEmail = users.some((obj) => obj.email === suEmail.value);
 
-    if (!searchForEmail) {
-        var user = {
-            name: suName.value,
-            email: suEmail.value,
-            password: suPassword.value
-        };
+    if(emailValidate.test(suEmail.value)&&passwordValidate.test(suPassword.value)&&nameValidate.test(suName.value)){
+        const searchForEmail = users.some((obj) => obj.email === suEmail.value);
 
-        users.push(user);
-
-        localStorage.setItem('users', JSON.stringify(users))
-
-        toggle.click();
-        Alert('success', 'Sign Up Successfully,', 'Thank you for registration');
-
-        clearSignUpData();
+        if (!searchForEmail) {
+            var user = {
+                name: suName.value,
+                email: suEmail.value,
+                password: suPassword.value
+            };
+    
+            users.push(user);
+    
+            localStorage.setItem('users', JSON.stringify(users))
+    
+            toggle.click();
+            Alert('success', 'Sign Up Successfully,', 'Thank you for registration.');
+    
+            clearSignUpData();
+        }
+        else {
+            Alert('danger', 'Sign Up Error,', 'The email you enterd is already signed up,Kindly login or use another email.');
+        }
     }
-    else {
-        Alert('danger', 'Sign Up Error,', 'The email you enterd is already signed up,Kindly login or use another email');
+    else{
+        Alert('danger', 'Sign Up Error,', 'Please follow input instructions to signup.');
     }
+
+    
 
 }
 
-
 function logIn() {
-    const searchedItem = users.find((obj) => obj.email === liEmail.value);
 
-    if (searchedItem) {
-        if (searchedItem.password == liPassword.value) {
-            loginSystem.classList.add('d-none');
-            data.classList.remove('d-none');
-            infoDisplay(searchedItem.name);
-            info.classList.replace('d-none', 'd-flex');
-            getData();
-            clearLogInData();
+    if(liEmail.value !='' && liPassword !=''){
+        const searchedItem = users.find((obj) => obj.email === liEmail.value);
+
+        if (searchedItem) {
+            if (searchedItem.password == liPassword.value) {
+                loginSystem.classList.add('d-none');
+                data.classList.remove('d-none');
+                infoDisplay(searchedItem.name);
+                info.classList.replace('d-none', 'd-flex');
+                getData();
+                clearLogInData();
+    
+                localStorage.setItem('status',`login,${searchedItem.name}`);
+            }
+            else {
+                Alert('danger',"Invalid Password,",'Please enter correct password!')
+            }
         }
         else {
-            Alert('danger',"Invalid Password,",'Please enter correct password')
+            Alert('danger',"Unregistered Email,",'Please sign up to go on!');
         }
     }
-    else {
-        Alert('danger',"Unregistered Email,",'Please sign up to go on');
+    else{
+        Alert('danger',"Empty Inputs,",'Please enter your email and password!');
     }
+   
 }
 
 loginBtn.addEventListener('click', logIn)
@@ -149,6 +228,9 @@ function logOut(){
     data.classList.add('d-none');
     loginSystem.classList.remove('d-none');
     info.classList.add('d-none');
+    
+
+    localStorage.setItem('status','logout');
 }
 
 
